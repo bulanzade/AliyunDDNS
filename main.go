ca2d731408b4e76c4ca902f94890de9afd9c96a1
bulan_zade <bulan_zade@qq.com>
2023-03-19 20:06:26 +0800

    replace 'log.Fatal' with 'log.Printf' to prevent unexpected exits

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
 	"github.com/go-co-op/gocron"
 	"io"
@@ -13,84 +12,108 @@ import (
 )
 
 type Config struct {
-	AccessIdKey	string
+	AccessIdKey  string
 	AccessSecret string
-	SubDomain string
-	Domain string
+	SubDomain    string
+	Domain       string
 }
 
-func readConfig() (config Config, err error) {
-	// 读取配置文件
-	readBytes, err := os.ReadFile("config.json")
+func readConfig(configFile string) (config Config, err error) {
+	readBytes, err := os.ReadFile(configFile)
 	if err != nil {
 		return
 	}
 	err = json.Unmarshal(readBytes, &config)
-	log.Printf("Config: %s\n", config)
 	return
 }
 
-func updateDomainRecord(config Config) {
-	// 获取本机ip
+func getIP() (ip string, err error) {
 	res, err := http.Get("https://6.ipw.cn")
 	if err != nil {
-		log.Fatal("Error when getting ip: ", err)
-	}
-	body, err := io.ReadAll(res.Body)
-	ip := string(body)
-	res.Body.Close()
-	if err != nil {
-		log.Fatal("Error when reading response from '6.ipw.cn': ", err)
-	}
-
-	client, err := alidns.NewClientWithAccessKey("cn-hangzhou", config.AccessIdKey, config.AccessSecret)
-	if err != nil {
-		log.Fatal(err)
 		return
 	}
-
-	// 获取子域名的记录
-	subDomainRecordsRequest := alidns.CreateDescribeSubDomainRecordsRequest()
-	subDomainRecordsRequest.Scheme = "https"
-	subDomainRecordsRequest.SubDomain = config.SubDomain + "." + config.Domain
-	describeSubDomainRecordsResponse, err := client.DescribeSubDomainRecords(subDomainRecordsRequest)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Fatal("Error when getting subDomain info: ", err)
+		return
 	}
-	log.Printf("Current record: %#v\n", describeSubDomainRecordsResponse.DomainRecords)
-
-	//如果子域名记录的ip与本机当前ip不同，就修改子域名解析记录
-	if describeSubDomainRecordsResponse.DomainRecords.Record[0].Value != ip {
-		//修改子域名解析记录
-		updateDomainRecordRequest := alidns.CreateUpdateDomainRecordRequest()
-		updateDomainRecordRequest.Scheme = "https"
-		updateDomainRecordRequest.Value = ip
-		updateDomainRecordRequest.Type = "AAAA"
-		updateDomainRecordRequest.RR = config.SubDomain
-		updateDomainRecordRequest.RecordId = describeSubDomainRecordsResponse.DomainRecords.Record[0].RecordId
-		response, err := client.UpdateDomainRecord(updateDomainRecordRequest)
-		if err != nil {
-			log.Fatal("Error when updating record", err)
-		}
-		log.Printf("Update RequestId: %s\n", response.RequestId)
-	}
+	ip = string(body)
+	err = res.Body.Close()
+	return
 }
 
-func print() {
-	fmt.Println("5s")
+func getSubDomainRecord(client *alidns.Client, subDomain string, domain string) (record alidns.Record, err error) {
+	request := alidns.CreateDescribeSubDomainRecordsRequest()
+	request.Scheme = "https"
+	request.SubDomain = subDomain + "." + domain
+	response, err := client.DescribeSubDomainRecords(request)
+	record = response.DomainRecords.Record[0]
+	return
+}
+
+func updateSubDomainRecord(client *alidns.Client, recordId string, value string, rr string) (requestId string, err error) {
+	request := alidns.CreateUpdateDomainRecordRequest()
+	request.Scheme = "https"
+	request.Type = "AAAA"
+	request.RR = rr
+	request.RecordId = recordId
+	request.Value = value
+	response, err := client.UpdateDomainRecord(request)
+	requestId = response.RequestId
+	return
+}
+
+func ddns(config Config, client *alidns.Client) {
+	// get ip
+	ip, err := getIP()
+	if err != nil {
+		log.Printf("Failed to get ip: %#v\n", err)
+		return
+	}
+	log.Printf("Current ip: %s\n", ip)
+	// get subdomain record
+	record, err := getSubDomainRecord(client, config.SubDomain, config.Domain)
+	if err != nil {
+		log.Printf("Failed to get subdomain record: %#v\n", err)
+		return
+	}
+	log.Printf("Current record: %#v\n", record)
+	// update record if ip has changed
+	if record.Value != ip {
+		requestId, err := updateSubDomainRecord(client, record.RecordId, ip, record.RR)
+		if err != nil {
+			log.Printf("Failed to update record: %#v\n", err)
+		}
+		log.Printf("Update reqeust id: %s\n", requestId)
+	}
 }
 
 func main() {
-	config, err := readConfig()
+	//set log
+	log.SetFlags(log.Llongfile | log.Lmicroseconds | log.Ldate)
+	logFile, err := os.OpenFile("ddns.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
-		log.Fatal("Error when reading config: ", err)
+		log.Fatalf("Failed to open log file: %#v\n", err)
+	}
+	log.SetOutput(logFile)
+	defer logFile.Close()
+
+	// read config
+	config, err := readConfig("config.json")
+	if err != nil {
+		log.Fatalf("Failed to read config file: %#v\n", err)
+	}
+	log.Printf("Current config: %#v\n", config)
+
+	// create client
+	client, err := alidns.NewClientWithAccessKey("cn-hangzhou", config.AccessIdKey, config.AccessSecret)
+	if err != nil {
+		log.Fatalf("Failed to create client: %#v", err)
 	}
 
 	s := gocron.NewScheduler(time.FixedZone("UTC+8", 0))
-	_, err = s.Every(10).Minutes().Do(updateDomainRecord, config)
+	_, err = s.Every(5).Minutes().Do(ddns, config, client)
 	if err != nil {
-		log.Fatal(err)
-		return 
+		log.Fatalln(err)
 	}
 	s.StartBlocking()
 }
